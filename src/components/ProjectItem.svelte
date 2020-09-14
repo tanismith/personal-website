3f372ee221fc89a9546609c9a90615d34724ee6f
tanismith <tanismith.design@gmail.com>
2020-09-13 21:44:45 -0500

    modify img size

--- src/components/ProjectItem.svelte
+++ src/components/ProjectItem.svelte
@@ -1,38 +1,44 @@
 <script>
-    import { each } from 'svelte/internal'
+  import { each } from "svelte/internal";
 
-    export let project
-    export let left = true
-    export let right = false
+  export let project;
+  export let left = true;
+  export let right = false;
 </script>
 
 <figure class="gallery-element" data-name={project.title}>
-    {#if left}
-        <a href={project.link || `projects/${project.slug}`} target="_blank">
-            <img src={project.thumb} alt={project.title} width="400" height="280" />
-        </a>
-    {/if}
+  {#if left}
+    <a href={project.link || `projects/${project.slug}`} target="_blank">
+      <img src={project.thumb} alt={project.title} />
+    </a>
+  {/if}
 
-    <div class="projectsFlexDescription">
-        <a href={project.link} target="_blank">
-            <h3>{project.title}</h3>
-        </a>
-        <p>
-            {#if project.tags}
-                {#each project.tags as tech, i}{tech} <span>{i === project.tags.length - 1 ? '' : '+ '}</span>{/each}
-            {/if}
+  <div class="projectsFlexDescription">
+    <a href={project.link} target="_blank">
+      <h3>{project.title}</h3>
+    </a>
+    <p>
+      {#if project.tags}
+        {#each project.tags as tech, i}
+          {tech}
+          <span>{i === project.tags.length - 1 ? '' : '+ '}</span>
+        {/each}
+      {/if}
 
-            <br />
-            {#if project.tools}
-                {#each project.tools as tool, i}{tool} <span>{i === project.tools.length - 1 ? '' : '+'}</span>{/each}
-            {/if}
-        </p>
-        <article>{project.overview}</article>
-        <a href={project.link || `projects/${project.slug}`}>See more</a>
-    </div>
-    {#if right}
-        <a href={project.link || `projects/${project.slug}`} target="_blank">
-            <img src={project.thumb} alt={project.title} width="400" height="280" />
-        </a>
-    {/if}
+      <br />
+      {#if project.tools}
+        {#each project.tools as tool, i}
+          {tool}
+          <span>{i === project.tools.length - 1 ? '' : '+'}</span>
+        {/each}
+      {/if}
+    </p>
+    <article>{project.overview}</article>
+    <a href={project.link || `projects/${project.slug}`}>See more</a>
+  </div>
+  {#if right}
+    <a href={project.link || `projects/${project.slug}`} target="_blank">
+      <img src={project.thumb} alt={project.title} />
+    </a>
+  {/if}
 </figure>
