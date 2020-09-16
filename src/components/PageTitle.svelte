179bc868d550d03b4321d29d5fac7121fe9aaaf6
tanismith <taniasmith.design@gmail.com>
2020-09-15 20:35:31 -0500

    icon

--- src/components/PageTitle.svelte
+++ src/components/PageTitle.svelte
@@ -1,19 +1,19 @@
 <script>
-    export let mode
-    export let title
-    export let strongTitle
-    export let description
+  export let mode;
+  export let title;
+  export let strongTitle;
+  export let description;
 </script>
 
-<div class="title-page-container">
-    {#if mode === 'home'}
-        <span>{title}</span>
-        <h1>{strongTitle}</h1>
-    {/if}
-    <hr />
-    {#if description}
-        <p>
-            {@html description}
-        </p>
-    {/if}
-</div>
+<h1 class="title-page-container">
+  {#if mode === 'home'}
+    <span>{title}</span>
+    <h1>{strongTitle}</h1>
+  {/if}
+  <hr />
+  {#if description}
+    <p>
+      {@html description}
+    </p>
+  {/if}
+</h1>
