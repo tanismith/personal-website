721517a83eb88712f999bf31b3436ab76c6237b9
tanismith <tanismith.design@gmail.com>
2020-09-23 17:58:32 -0500

    footer icon

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,10 +1,12 @@
 <footer>
-    <section>
-        <p>Connect with me at</p>
-        <hr />
-    </section>
-    <section>
-        <a href="https://www.linkedin.com/in/tania-smith-senmache"> <i class="fab fa-linkedin-in" /> </a>
-        <a href="https://github.com/tanismith"> <i class="fab fa-github" /> </a>
-    </section>
+  <section>
+    <p>Connect with me at</p>
+    <hr />
+  </section>
+  <section>
+    <a href="https://www.linkedin.com/in/tania-smith-senmache">
+      <i class="fab fa-linkedin-in" /></a>
+    <a href="https://github.com/tanismith"> <i class="fab fa-github" /></a>
+    <a href="https://dribbble.com/TaniSmith"> <i class="fab fa-dribbble" /></a>
+  </section>
 </footer>
