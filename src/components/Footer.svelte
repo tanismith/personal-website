fb30bd43d50dc763a3ecb0628b8f362c9a1ecef8
tanismith <tanismith.design@gmail.com>
2020-09-23 18:23:01 -0500

    footer icon

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -7,6 +7,8 @@
     <a href="https://www.linkedin.com/in/tania-smith-senmache">
       <i class="fab fa-linkedin-in" /></a>
     <a href="https://github.com/tanismith"> <i class="fab fa-github" /></a>
-    <a href="https://dribbble.com/TaniSmith"> <i class="fab fa-dribbble" /></a>
+    <a
+      href="https://codepen.io/your-work/?cursor=ZD0wJm89MSZwPTEmdj00NTI1MDgyNg==">
+      <i class="fab fa-codepen" /></a>
   </section>
 </footer>
