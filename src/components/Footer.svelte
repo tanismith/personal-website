692a0aeb1be9088725b21af240e55cd77aba6275
tanismith <tanismith.design@gmail.com>
2020-09-23 19:06:17 -0500

    footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -7,8 +7,7 @@
     <a href="https://www.linkedin.com/in/tania-smith-senmache">
       <i class="fab fa-linkedin-in" /></a>
     <a href="https://github.com/tanismith"> <i class="fab fa-github" /></a>
-    <a
-      href="https://codepen.io/your-work/?cursor=ZD0wJm89MSZwPTEmdj00NTI1MDgyNg==">
+    <a href="https://codepen.io/t4n1a/pens/showcase">
       <i class="fab fa-codepen" /></a>
   </section>
 </footer>
