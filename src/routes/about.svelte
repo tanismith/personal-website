af4d244ed1a8a072db925c59d926c9769739ec64
tanismith <tanismith.design@gmail.com>
2020-09-24 22:17:09 -0500

    about

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -2,15 +2,26 @@
   import PageTitle from "../components/PageTitle.svelte";
 </script>
 
-<PageTitle mode="home" title="About" description="" />
+<PageTitle mode="home" title="Hi there," description="" />
 <div class="about">
   <div class="aboutDescription">
     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quia,
-      numquam id voluptatibus officiis repellendus veritatis a nulla
-      consequuntur facere, eos eum exercitationem cum voluptas ipsam vitae iusto
-      consectetur ipsum.
+      I enjoy building digital products and working in projects wich are
+      challenging. I move from concepts and sketches, to wireframes and
+      prototypes, to high fidelity UI/UX designs. <br /><br />I am interested in
+      ways to transform areas where there are gaps, to be more inclusive,
+      accessible and humane. My commitment is to contribute to raise awareness
+      in the creation of designs focused on the needs of users. <br /><br /> With
+      my work I hope to simplify user's everyday lives with a beautiful user experience.
+      One of my pillars for creating solutions is the perspective of self-reflection
+      and investigation accompanied by artistic sensitivity. I love moving from design
+      to implementing interactive experiences on the web, bringing ideas to life
+      with meticulous care and attention to detail. <br /><br /> A little more about
+      me <br />My personal experiences have taught me a few things about myself:
+      I constantly educate myself, readjust and train to be a better person. I
+      work daily to form positive habits. I'm always looking for new and better
+      ways to solve problems.
     </p>
-    <img src="/images/about.jpg" alt="tania-smith-senmache" />
+    <!-- <img src="/images/about.jpg" alt="tania-smith-senmache" /> -->
   </div>
 </div>
